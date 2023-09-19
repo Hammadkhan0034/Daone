8269b2d3585c9582f1762cb111ee9c316e2b58f2
osamahassank <117932781+Hammadkhan0034@users.noreply.github.com>
2023-09-20 04:14:37 +0500

    more update

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      popGesture: false,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           visualDensity: VisualDensity.standard,
